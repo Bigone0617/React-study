3d417f319318034a7f993b0ecc713c9ffdf5d3f8
Bigone0617 <xodlfsha@gmail.com>
2023-10-23 22:59:51 +0900

    state 초기 할당 && 사용하지 않아도 에러는 안남

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-class App extends React.Component<{name: string;}> {
+class App extends React.Component<{name: string;}, {age: number}> {
   render(){
     return ( 
       <div className='App'>{this.props.name}</div>
