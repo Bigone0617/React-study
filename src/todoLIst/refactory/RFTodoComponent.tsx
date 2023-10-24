3eac5e0b770c93fc50f50c7c93ad253f8d098984
Bigone0617 <xodlfsha@gmail.com>
2023-10-24 23:54:55 +0900

    수정할 때만, input 박스 보이게 수정

--- src/todoLIst/refactory/RFTodoComponent.tsx
+++ src/todoLIst/refactory/RFTodoComponent.tsx
@@ -12,8 +12,9 @@ interface ChangeTodoListProps {
     todo: TodoProps;
 }
 
-const RFTodoComponent: React.FC<ChangeTodoListProps> = ({ removeTodoList, updateTodoList, todo }) => {
+const TodoComponent: React.FC<ChangeTodoListProps> = ({ removeTodoList, updateTodoList, todo }) => {
     const [localTodo, setLocalTodo] = useState<TodoProps>(todo);
+    const [isEditing, setIsEditing] = useState(false);
 
     useEffect(() => {
         setLocalTodo(todo);
@@ -27,18 +28,33 @@ const RFTodoComponent: React.FC<ChangeTodoListProps> = ({ removeTodoList, update
         });
     }
 
+    const toggleEdit = (): void => {
+        setIsEditing(!isEditing);
+    }
+
     const updateTodoHandler = (): void => {
         updateTodoList(localTodo);
+        toggleEdit();
     }
 
     return (
         <div>
-            <input id="title" value={localTodo.title} onChange={onChangeHandler}></input>
-            <input id="description" value={localTodo.description} onChange={onChangeHandler}></input>
-            <button onClick={updateTodoHandler}>수정</button>
+            {isEditing ? (
+                <>
+                    <input id="title" value={localTodo.title} onChange={onChangeHandler}></input>
+                    <input id="description" value={localTodo.description} onChange={onChangeHandler}></input>
+                    <button onClick={updateTodoHandler}>수정</button>
+                </>
+            ) : (
+                <>
+                    <span>{localTodo.title}</span>
+                    <span>{localTodo.description}</span>
+                    <button onClick={toggleEdit}>수정</button>
+                </>
+            )}
             <button onClick={() => removeTodoList(localTodo.id)}>삭제</button>
         </div>
     );
 }
 
-export default RFTodoComponent;
+export default TodoComponent;
