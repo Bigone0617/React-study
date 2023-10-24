12ba2b592479cfc01227007ff09754fe047b6868
tell.kim <tell.kim@kmpay.co.kr>
2023-10-24 09:28:53 +0900

    props에 default 값 설정하기

--- src/App.tsx
+++ src/App.tsx
@@ -6,6 +6,7 @@ import LifeCycle from './LifeCycle';
 export interface AppProp {
   name: string,
   children?: React.ReactNode; // children을 optional로 변경
+  company?: string;
 }
 
 interface Appstate {
@@ -13,6 +14,10 @@ interface Appstate {
 } 
 
 class App extends React.Component<AppProp, Appstate> {
+  // optional props에 기본값 지정하기.
+  static defaultProps = {
+    company: 'Apple'
+  }
   constructor(prop: AppProp) {
     super(prop)
     this.state = {
@@ -30,10 +35,13 @@ class App extends React.Component<AppProp, Appstate> {
     return ( 
       <>
         <>
-          <div className='App'>{this.props.name} {this.state.age}</div>
+          <div className='App'>{this.props.name} {this.props.company} {this.state.age}</div>
           <StateLessComponent name="anna">
             <h2>hihi</h2>
           </StateLessComponent>
+          <StateLessComponent2 name='ho'>
+            <div>123</div>
+          </StateLessComponent2>
         </>
         <>
           <LifeCycle job="developer"/>
@@ -48,10 +56,24 @@ class App extends React.Component<AppProp, Appstate> {
 const StateLessComponent: React.FC<AppProp> = (props) => {
   return (
     <>
-      <h2>{props.name}</h2>
+      <h2>{props.name} {props.company}</h2>
       {props.children}
     </>
   )
 }
 
+StateLessComponent.defaultProps = {
+  company: 'Home'
+}
+
+
+
+const StateLessComponent2: React.FC<AppProp> = ({name, children, company = "Home2"}) => {
+  return (
+    <>
+      <h2>{name} {company}</h2>
+      {children}
+    </>
+  )
+}
 export default App;
