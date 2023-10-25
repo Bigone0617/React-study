f7ab3a6ac696a772282ed0edd5b810d9995c18ad
Bigone0617 <xodlfsha@gmail.com>
2023-10-26 00:15:20 +0900

    how to get props at Route element

--- src/ReactRouter/RouterHome.tsx
+++ src/ReactRouter/RouterHome.tsx
@@ -1,5 +1,15 @@
 import React from 'react';
-import { BrowserRouter as Router, Route, Link, Routes } from 'react-router-dom';
+import { BrowserRouter as Router, Route, Link, Routes, useParams } from 'react-router-dom';
+
+
+const Post = () => {
+    // Route의 element로 넘어오는 props를 가져오는 방식
+    const params = useParams();
+    
+    return (
+        <h2>Post {params.postId}</h2>
+    )
+}
 
 class RouterHome extends React.Component<{}, {}> {
     render(){
@@ -8,6 +18,13 @@ class RouterHome extends React.Component<{}, {}> {
                 <Routes>
                     <Route path="/" element={<h3>home</h3>}/>
                     <Route path="/intro" element={<h3>intro</h3>}/>
+                    <Route 
+                        path="/posts/:postId" 
+                        // loader={({ params }) => {
+                        //     console.log(params);
+                        // }}
+                        element={<Post />}
+                    />
                 </Routes>
                 <nav>
                     <ul>
