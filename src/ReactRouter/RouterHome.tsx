9967b8f8519a7ba42741d9f36dc35befb8f8915d
tell.kim <tell.kim@kmpay.co.kr>
2023-10-26 13:24:23 +0900

    v6 react router dom : get params-query params and go another route

--- src/ReactRouter/RouterHome.tsx
+++ src/ReactRouter/RouterHome.tsx
@@ -1,13 +1,37 @@
 import React from 'react';
-import { BrowserRouter as Router, Route, Link, Routes, useParams } from 'react-router-dom';
+import { BrowserRouter as Router, Route, Link, Routes, useParams, useLocation, useNavigate, useMatch, useMatches, useSearchParams } from 'react-router-dom';
 
+interface PostProps {
+    name : string;
+}
 
-const Post = () => {
-    // Route의 element로 넘어오는 props를 가져오는 방식
-    const params = useParams();
+// ...props를 이용하면 컴퍼넌트로 넘어오는 모든 props가 표현됨
+// v6 이전에는 props의 type이 RouteComponentProps 였음.
+const Post:React.FC<PostProps> = ({...props}) => {
+
+    // url 네비게이션
+    // v6전에는 props.history.push를 사용해야 됨.
+    const navitage = useNavigate();
+    const goNextPost = () => {
+        navitage('/posts/'+(Number(params.postId)+1))
+    }
+
+    // query string을 얻기 위해서는 이 함수를 사용.
+    // ex) ...?age=20;
+    const [searchParams] = useSearchParams();
+    const age = searchParams.get('age'); // 20
+
+    // Route의 element로 넘어오는 url params를 가져오는 방식
+    // v6 전에는 props를 이용해서 받아옴.
+    // ex) .../posts/:postId -> .../posts/30
+    const params = useParams(); // 30
     
     return (
-        <h2>Post {params.postId}</h2>
+        <div>
+            <h2>Post {params.postId} {props?.name}</h2>
+            <h2>queryString : {age}</h2>
+            <button onClick={goNextPost}>다음 포스트</button>            
+        </div>
     )
 }
 
@@ -20,10 +44,7 @@ class RouterHome extends React.Component<{}, {}> {
                     <Route path="/intro" element={<h3>intro</h3>}/>
                     <Route 
                         path="/posts/:postId" 
-                        // loader={({ params }) => {
-                        //     console.log(params);
-                        // }}
-                        element={<Post />}
+                        element={<Post name="kim"/>}
                     />
                 </Routes>
                 <nav>
